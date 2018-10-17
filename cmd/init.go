77228c9e0f2fdb5c841af1013e4f9cdc9397d8df
Nic Westvold <nwestvold@powerchordsystem.com>
2018-10-17 00:46:45 -0400

    adds init command
    updates version command

--- cmd/init.go
+++ cmd/init.go
@@ -16,30 +16,49 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "",
-	Long:  ``,
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize i18n files for a directory",
+	Long:  `A "en-US.json" file is created in the root directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.2")
+		str := []byte("{\n}\n")
+
+		if _, err := os.Stat("en-US.json"); !os.IsNotExist(err) {
+			fmt.Println("file exists. skipping init")
+		} else {
+			f, err := os.Create("en-US.json")
+			check(err)
+
+			_, err = f.Write(str)
+			check(err)
+
+			defer f.Close()
+			fmt.Println("i18n initialized")
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(initCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
